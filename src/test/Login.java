a66b5201a35a7c02e1df08cba6c56fde112f186e
dukala <dukala0518@gmail.com>
2024-03-14 12:43:40 -0500

    update

--- src/test/Login.java
+++ src/test/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("choose your item");
         System.out.println("Purchase it");
+        System.out.println("Total 5000");
     }
 }
